d41cb9c67b30669c7efa9e8dc42fdc222fa92605
Pedro Marcano <pedromarcdev@gmail.com>
2025-06-15 17:12:22 -0400

    Add function for create cookies and validate banner display

--- modules/cookiesbanner/views/js/front.js
+++ modules/cookiesbanner/views/js/front.js
@@ -27,5 +27,58 @@
 */
 
 document.addEventListener('DOMContentLoaded', function() {
-    
+    const cookiesBanner = document.querySelector('#cookiesBanner');
+    const acceptCookies = document.querySelector('#cookiesAccepted');
+    const rejectCookies = document.querySelector('#cookiesRejected');
+    const popupOverlay = document.querySelector('.cookies-overlay');
+
+    if (acceptCookies) {
+        acceptCookies.addEventListener('click', (e) => {
+            e.preventDefault();
+            setCookie('consent_cookies', 'accepted', 365);
+
+            cookiesBanner.style.display = 'none';
+            if (popupOverlay) popupOverlay.style.display = 'none';
+        })
+    }
+
+    if (rejectCookies) {
+        rejectCookies.addEventListener('click', (e) => {
+            e.preventDefault();
+            setCookie('consent_cookies', 'rejected', 365);
+
+            cookiesBanner.style.display = 'none';
+            if (popupOverlay) popupOverlay.style.display = 'none';
+        })
+    }
+
+    if (getCookie('consent_cookies') !== '') {
+        cookiesBanner.style.display = 'none';
+        if (popupOverlay) popupOverlay.style.display = 'none';
+        return;
+    }
+
+    function setCookie(name, value, days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    }
+
+    function getCookie(name) {
+        const cookieName = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        
+        for(let i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length, cookie.length);
+            }
+        }
+        return '';
+    }
 });
--- modules/cookiesbanner/views/templates/hook/displayFooterAfter.tpl
+++ modules/cookiesbanner/views/templates/hook/displayFooterAfter.tpl
@@ -2,15 +2,15 @@
     <div class="cookies-overlay"></div>
 {/if}
 
-<div class="cookies-banner {$banner_position}" style="background: {$bg_color};">
+<div class="cookies-banner {$banner_position}" id="cookiesBanner" style="background: {$bg_color};">
     <h3 style="color: {$title_color};">{$title}</h3>
     <p style="color: {$txt_color};">{$content}</p>
     
     <div class="cookies-buttons">
-        <button class="accept-cookies" style="color: {$accept_txt_color}; background: {$accept_bg_color}">
+        <button class="accept-cookies" id="cookiesAccepted" style="color: {$accept_txt_color}; background: {$accept_bg_color}">
             {$txt_btn_accept}
         </button>
-        <button class="refuse-cookies" style="color: {$refuse_txt_color}; background: {$refuse_bg_color}">
+        <button class="refuse-cookies" id="cookiesRejected" style="color: {$refuse_txt_color}; background: {$refuse_bg_color}">
             {$txt_btn_refuse}
         </button>
     </div>
